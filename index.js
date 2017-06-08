741afcee21cc72e68979c1d17131835536148dd9
Landon Reed <landontreed@gmail.com>
2017-06-08 11:31:28 -0400

    docs(url-param): add default value for url param

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const searchUrl = `${mapzenUrl}/search`
  *   {@link https://mapzen.com/documentation/search/autocomplete/#layers|layer types}
  * @param  {string} [$0.sources='gn,oa,osm,wof']
  * @param  {string} $0.text                       query text
- * @param {string} [$0.url]                       optional URL to override Mapzen autocomplete endpoint
+ * @param {string} [$0.url='https://search.mapzen.com/v1/autocomplete']                       optional URL to override Mapzen autocomplete endpoint
  * @return {Promise}                              A Promise that'll get resolved with the autocomplete result
  */
 export function autocomplete ({
@@ -94,7 +94,7 @@ export function autocomplete ({
  * @param {number} [$0.size=10]
  * @param {string} [$0.sources='gn,oa,osm,wof']
  * @param {string} $0.text                      The address text to query for
- * @param {string} [$0.url]                     optional URL to override Mapzen search endpoint
+ * @param {string} [$0.url='https://search.mapzen.com/v1/search']                     optional URL to override Mapzen search endpoint
  * @return {Promise}                            A Promise that'll get resolved with search result
  */
 export function search ({
@@ -150,7 +150,7 @@ export function search ({
  * @param {string} $0.apiKey                    The Mapzen API key
  * @param {boolean} $0.format
  * @param {{lat: number, lon: number}} $0.point Point to reverse geocode
- * @param {string} [$0.url]                     optional URL to override Mapzen reverse endpoint
+ * @param {string} [$0.url='https://search.mapzen.com/v1/reverse']                     optional URL to override Mapzen reverse endpoint
  * @return {Promise}                            A Promise that'll get resolved with reverse geocode result
  */
 export function reverse ({
